752d7ecd618f45287233af2a2c884393dadbbf08
ChristianBaculima <bacup1394@hotmail.com>
2019-06-18 14:07:46 -0500

    Add files via upload

---
+++ Proyecto/src/main/java/entidadesNegocio/LoginEN.java
@@ -0,0 +1,29 @@
+package entidadesNegocio;
+
+public class LoginEN {
+	
+	//atributos de Login
+	private String codigo;
+
+	
+	//Getters y Setters
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public void ingresar() {
+		//metodo para ingresar al sistema
+	}
+
+	public void consultarHoraSalida() {
+		//metodo para consultar la hora de salida del reprsentado
+	}
+
+	public void consultarHoraLlegada() {
+		//metodo para consultar la hora de llegada del reprsentado
+	}
+}
---
+++ Proyecto/src/main/java/entidadesNegocio/TipoPAgoEN.java
@@ -0,0 +1,23 @@
+package entidadesNegocio;
+
+public class TipoPAgoEN {
+
+	//atributos de TipoPago
+	private String codigo;
+	private String descripcion;
+
+	//Getters y Setters
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
